032496ce1aee0a75e3e9ea5b38dd024db4c7c0a2
josecortorreal <josebibby23@gmail.com>
2023-02-16 23:00:51 -0500

    lowercase and length prompt done

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
+let lowercase = "qwertyuioplkjhgfdsazxcvbnm".split('');
+let uppercase = "qwertyuioplkjhgfdsazxcvbnm".toUpperCase().split('');
+let numbers = "1234567890".split();
+let specialChars = "!@#$%^&*()_+=-".split();
+let password = "";
 
 // Write password to the #password input
   // let password = generatePassword(); {
@@ -10,10 +15,18 @@ let generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   let length = prompt("What length do you want your password(8-128 characters): ");
+  
+  let lowercasePrompt = prompt("Do you want to include lowercase characters(Yes or No): ");
+  //doing this so that I could have at least one lower case letter in password
+  if(lowercasePrompt.toLowerCase() === "yes")  {
+    password.concat(generate(lowercase));
+  }
+
 }
 
 function generate(arr) {
-
+  let randomChar = arr[Math.floor(Math.random() * arr.length)]
+  console.log(randomChar);
 }
 
 function completePassword(arr, length) {
